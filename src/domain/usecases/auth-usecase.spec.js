f20910a3e0b156500f4f17add7c287a7be6b4c11
Joan Pedro Oliveira de Souza <jpos.senai@gmail.com>
2020-06-27 11:37:54 -0300

    Ensure AuthUseCase throws if no dependencies is provided

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -82,6 +82,12 @@ describe('Auth UseCase', () => {
     await expect(promise).rejects.toThrow()
   })
 
+  test('Should throw if no dependency is provided', async () => {
+    const sut = new AuthUseCase()
+    const promise = sut.auth('any_email@email.com', 'any_password')
+    await expect(promise).rejects.toThrow()
+  })
+
   test('Should throw if LoadUserByEmailRepository has no load method', async () => {
     class LoadUserByEmailRepositorySpy {}
     const loadUserByEmailRepositorySpy = new LoadUserByEmailRepositorySpy()
