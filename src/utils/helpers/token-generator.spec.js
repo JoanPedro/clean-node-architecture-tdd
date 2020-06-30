947f526e2bcad67c2fab62a1a2a7c5287e48362b
Joan Pedro Oliveira de Souza <jpos.senai@gmail.com>
2020-06-30 06:55:08 -0300

    Ensure TokenGenerator returns a token if JWT returns token

--- src/utils/helpers/token-generator.spec.js
+++ src/utils/helpers/token-generator.spec.js
@@ -1,13 +1,22 @@
+const jwt = require('jsonwebtoken')
+
 class TokenGenerator {
   async generate (id) {
-    return null
+    return jwt.sign(id, 'secretKey')
   }
 }
 
 describe('Token Generator', () => {
   test('Sould return null if JWT returns null', async () => {
     const sut = new TokenGenerator()
+    jwt.token = null
     const token = await sut.generate('any_id')
     expect(token).toBeNull()
+  })
+
+  test('Sould return a token if JWT returns token', async () => {
+    const sut = new TokenGenerator()
+    const token = await sut.generate('any_id')
+    expect(token).toBe(jwt.token)
   })
 })
