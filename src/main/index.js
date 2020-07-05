01d6bc8b296dc2773a4c49c081faf97d76292272
Joan Pedro Oliveira de Souza <jpos.senai@gmail.com>
2020-07-05 11:24:44 -0300

    Move LoginRouterComposition to its own file

--- src/main/index.js
+++ src/main/index.js
@@ -4,7 +4,6 @@ const env = require('./config/env')
 MongoHelper.connect(env.mongoUrl)
   .then(() => {
     const app = require('./config/app')
-    console.log('Oi')
     app.listen(5858, () => console.log('Server Running'))
   })
   .catch(() => console.error)
